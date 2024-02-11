e8b48b48106af77656067be9c219564ebc815e1a
Estefany Hernández Castro <153030320+EstefanyHdzCastro@users.noreply.github.com>
2024-02-11 13:05:01 -0600

    Add files via upload

---
+++ Variables.R
@@ -0,0 +1,50 @@
+
+###Types###
+
+#integer
+x <- 2L
+typeof(x)
+
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z<- 3+ 2i
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+#logical
+ q1 <- T
+typeof(q1) 
+
+q2<-FALSE
+typeof(q2)
+
+###Using variables###
+A <-10
+B<- 5
+
+C <- A+ B
+C
+
+#variable 1
+var1 <-2.5
+#variable 2
+var2 <-4
+
+result <- var1/var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "Hello"
+name <-"Bob"
+message <- paste(greeting, name)
+message
+
